be7f6b6fc3dbbfd5a04b1d1c7d40e18709b495f4
Saloni <salonimehta2127@gmail.com>
2021-05-02 17:17:15 -0400

    commit tree 1736ecd37fabe52eb2ba15f38c2ee003b2484b8a

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName='bob';
+
+function upperCaseCustomerName()
+{
+    return customerName= customerName.toUpperCase();
+
+}
+function setBestCustomer()
+{
+    bestCustomer='not bob';
+}
+function overwriteBestCustomer()
+{
+    return bestCustomer='maybe bob';
+}
+const leastFavoriteCustomer="Lisa";
+
+function changeLeastFavoriteCustomer()
+{
+ leastFavoriteCustomer="Joann";
+}
